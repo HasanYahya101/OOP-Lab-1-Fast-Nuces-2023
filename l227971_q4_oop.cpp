2873428c94d07ab1fc1f3174acd06f2b0ab9a0be
hasnayahya101 <118683092+hasnayahya101@users.noreply.github.com>
2023-02-03 02:20:04 +0500

    Solution files in C++/cpp

---
+++ l227971_q4_oop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int height, width;
+	cout << "Enter Height: ";
+	cin >> height;
+	cout << "Enter Width: ";
+	cin >> width;
+	int *firstp;
+	int* secondp;
+	firstp = &height;
+	secondp = &width;
+	cout << "The total area is: " << *firstp * *secondp << endl;
+	return 0;
+}
